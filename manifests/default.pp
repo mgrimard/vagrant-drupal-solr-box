e5c0faa6ac30cdc5c9e3b0dfc58dcf20a2eb82df
Dave Reid <dave@davereid.net>
2014-04-08 11:13:17 -0500

    Switch to using archive.apache.org since they don't remove old versions from downloads.

--- manifests/default.pp
+++ manifests/default.pp
@@ -74,7 +74,7 @@ class must-have {
   }
 
   exec { "download_solr":
-    command => "curl -L http://mirrors.ibiblio.org/apache/lucene/solr/4.7.1/solr-4.7.1.tgz | tar zx --directory=/vagrant/solr --strip-components 1",
+    command => "curl -L http://archive.apache.org/dist/lucene/solr/4.7.1/solr-4.7.1.tgz | tar zx --directory=/vagrant/solr --strip-components 1",
     cwd => "/vagrant",
     user => "vagrant",
     path => "/usr/bin/:/bin/",
